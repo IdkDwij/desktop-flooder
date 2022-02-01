9b723a03939cd37140c2e9060b1059de2220f87c
Dwij <94942567+IdkDwij@users.noreply.github.com>
2022-02-01 17:17:29 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+import threading
+
+
+def makefile():
+    thing = random.randint(1, 100000)
+    number = thing
+    f = open('file.txt' + str(number), "w")
+    f.write(':))))))')
+    f.close()
+
+
+threads = 512
+while threads >= 0:
+    print('making file')
+    thread = threading.Thread(target=makefile())
+    thread.start()
+    threads -= 1
+
+input('test')
